53463091f1545a23df69b5f78fe6d0faa23277e6
Admin <georg-edward.piil@voco.ee>
2025-10-15 05:13:35 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// console.log  ('Tere maailm!');
+// window.alert('Mulle ei maitse maks!')
+
+// Kommentaar
+/*
+Multirealine kommentaar
+*/
+
+document.getElementById('myH1').textContent = 'Tere jälle Georg!';
+document.getElementById('myP').textContent = 'See on minu esimene leht.';
+
+// Muutuja = konteiner, kuhu saab salvestada andmeid
+// Käitub nagu silt, millele on kirjutatud mingi info
+// Muutuja nimeks võib panna suvalise sõna, aga peab järgima reegleid
+// Muutuja nimi ei tohi alata numbriga
+
+let x=5;
+
+
+let age = 23;
+let price=10.99;
+
+console.log(x);
+console.log (price);
+console.log(age);
+console.log(`Ma olen ${age} aastat vana ja õpin programmeerimist. Hind on ${price} eurot.`);
