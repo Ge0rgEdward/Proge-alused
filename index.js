3763f4e8246ff6de2717d090ce1ffc9d029ed59a
Admin <georg-edward.piil@voco.ee>
2025-10-23 05:04:16 -0700

    Vanuse analüüs ÜL

--- index.js
+++ index.js
@@ -328,3 +328,27 @@ console.log(`Your total is $${purchaseAmount-purchaseAmount*(discount/100)}`);
 
 
 
+// Ülesanne: Vanuse analüüs
+
+let õpilaseVanus;
+
+mySubmit.onclick = function() {
+    õpilaseVanus = myText.value;
+    õpilaseVanus = Number(õpilaseVanus);
+
+if (õpilaseVanus >= 7 && õpilaseVanus <=15) {
+    resultElement.textContent = `Sa oled Põhikooli õpilane- söömine toimub kell 10!`;
+}
+else if (õpilaseVanus >=17 && õpilaseVanus <=20) {
+    resultElement.textContent = `Sa oled gümnaasiumi õpilane- tule sööma kell 11!`;
+}
+else if (õpilaseVanus >20) {
+    resultElement.textContent = `Täiskasvanud söövad kell 12!`;
+}
+else if (õpilaseVanus <7) {
+    resultElement.textContent = `Sa oled liiga noor, et koolis käia. Mine lasteaeda tagasi!`;
+}
+else {
+    resultElement.textContent = `Söömis aja kontrollimiseks sisesta palun oma vanus! See peab olema number.`;
+}
+}
